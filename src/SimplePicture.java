793495a635dede7156eae1b9be991e69cd7eff37
NEGAR <n_ghoodjani@u.pacific.edu>
2026-02-13 11:03:08 -0800

    BLAH BLAH BLAH

--- src/SimplePicture.java
+++ src/SimplePicture.java
@@ -13,7 +13,11 @@ public class SimplePicture extends GraphicsProgram {
 	}
 	//Worms time.
 	public void run() { //Runs the program
+<<<<<<< Upstream, based on branch 'master' of https://github.com/ClassCOMP55/git-team-lab-team-2.git
 		GImage robot = new GImage(IMAGE_FRIEND, 200, 400);
+=======
+		GImage robot = new GImage(IMAGE_FILE, 200, 350);
+>>>>>>> ba88bd4 CHANGED
 		add(robot);
 		GLabel label = new GLabel("How I feel when I wasn't using gitter", 300, 300);
 		add(label);
