9fbe3c4e0a4934004e3936ad355f90a45a6b5557
cFranks2 <c_franks2@u.pacific.edu>
2026-02-13 10:41:36 -0800

    The Horrors.

--- src/SimplePicture.java
+++ src/SimplePicture.java
@@ -7,17 +7,17 @@ import acm.program.GraphicsProgram;
 public class SimplePicture extends GraphicsProgram {
 	private static final String IMAGE_FILENAME = "robot.png";
 	public static int SIZE = 700;
-	
+	//I love causing conflict
 	public void init() {
 		setSize(SIZE, SIZE);
 	}
-
+	//Worms time.
 	public void run() { //Runs the program
 		GImage robot = new GImage(IMAGE_FILENAME, 200, 200);
 		add(robot);
 		GLabel label = new GLabel("How I feel when I wasn't using github", 300, 300);
 		add(label);
-	}
+	} //Worms time
 
 	public static void main(String[] args) {
 		new SimplePicture().start();
