bfe321f27b9c7a1c65b3bcdf04e69bf23381c6db
Lance Vang <l_vang21@u.pacific.edu>
2026-02-13 10:32:48 -0800

    2nd commit

--- src/SimplePicture.java
+++ src/SimplePicture.java
@@ -3,6 +3,7 @@ import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
 
 //Hi adding commit
+//commit 2
 public class SimplePicture extends GraphicsProgram {
 	private static final String IMAGE_FILENAME = "robot.png";
 	public static int SIZE = 700;
